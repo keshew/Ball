123da94be3624acbc5b0c137cf864bac9ca41c31
keshew <artyom.korotckov2017@yandex.ru>
2025-01-20 23:44:46 +0500

    Initial Commit

---
+++ Ball/BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallApp.swift
+//  Ball
+//
+//  Created by Артём Коротков on 20.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
